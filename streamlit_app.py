f69879be70bef7182383b2557e35fc77c446c050
AminAli-DS <66701535+AminAli-DS@users.noreply.github.com>
2021-12-30 20:35:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def inference(row, scaler, model, feat_cols):
     features = pd.DataFrame(X, columns = feat_cols)
     if (model.predict(features)==0):
         return "Ви здорова людіна!"
-    else: return "У вас великі шанси захворіти на діабет, зверніться до лікаря!"  
+    else: return "У вас великі шанси захворіти на діабет, зверніться до лікаря! (Здорові показники для тесту -  34, 2, 13, 4, 30, 6, 5, 0.55)"  
     
 
 age =           st.sidebar.number_input("Вік", 1, 150, 25, 1)
